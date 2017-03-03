a7e88d6a1a0e19b69a16527c2ece32c8ca7799a8
Przemysław Mokwa <mokwaprzemek@gmail.com>
2017-03-03 17:50:22 +0100

    Aktualizacja kodu 3 marca 2017 17:50

--- compa/compa.py
+++ compa/compa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#-*- coding: windows-1250 -*-
+#-*- coding: utf-8 -*-
 
 import os
 import sys
@@ -14,36 +14,36 @@ counter_difrence = 0
 def print_help():
 	print "..........:::::::::: COMPA - RER ::::::::::.........."
 	print
-	print u"Autor: Przemys³aw Mokwa"
-	print u"Licencja: Open-source"
+	print "Autor: Przemyslaw Mokwa"
+	print "Licencja: Open-source"
 	print
-	print u"Za pomoc¹ tego programu mo¿esz porównaæ dwa pliki pod wzglêdem zawartoœci,"
-	print u"znaleŸæ te ró¿nice krok po kroku, lub wygenerwowaæ plik z zmianami wzglêdem orygina³u"
-	print u"oraz plik z logami zmian. Celem programu jest u¿ytecznoœæ i szybkie dzia³anie."
-	print u"Po co? Poniewa¿ ten program jest Ci potrzebny gdy w ogromnym pliku kodu programu szukasz literówki,"
-	print u"a nie masz dostêpu do swojego ulubionego œrodowiska"
+	print "Za pomoca tego programu mozesz porownac dwa pliki pod wzgledem zawartosci,"
+	print "znalezc te roznice krok po kroku, lub wygenerwowac plik z zmianami wzglêdem oryginalu"
+	print "oraz plik z logami zmian. Celem programu jest uzytecznoœæ i szybkie dzialanie."
+	print "Po co? Poniewaz ten program jest Ci potrzebny gdy w ogromnym pliku kodu programu szukasz literowki,"
+	print "a nie masz dostepu do swojego ulubionego srodowiska"
 	print
-	print u"Wywo³anie programu: "
-	print u"compo.py OPTIONS <filename_org> <filename_compare>"
-	print u"Dostêpne opcje: "
-	print u"compa.py -h , --help\t==> Wyœwietlenie pomocy"
-	print u"compa.py -n , --no-line\t==> Program nie bierze pod uwagê numeracji linii. Porównuje tylko zawartoœæ tekstu"
-	print u"compa.py -l , --line-on\t==> Program bierze pod uwagê numeracje lini i opcja wchodzi w sk³adnik do porównania"
-	print u"compa.py -g <filename> , --genere <filename>\t==> Program tworzy poprawiony plik wzglêdem orygina³u"
-	print u"compa.py -o <filename> , --output-log <filename>\t==> Utworzenie pliku z logami zmian"
-	print u"compa.py -d , --debug\t==> W³¹cza dodatkowe informacje (zwi¹zane z prac¹ programu, nie wykonywanymi zadaniami)"
+	print "Wywolanie programu: "
+	print "compo.py OPTIONS <filename_org> <filename_compare>"
+	print "Dostepne opcje: "
+	print "compa.py -h , --help\t==> Wyswietlenie pomocy"
+	print "compa.py -n , --no-line\t==> Program nie bierze pod uwage numeracji linii. Porownuje tylko zawartosc tekstu"
+	print "compa.py -l , --line-on\t==> Program bierze pod uwage numeracje linii i opcja wchodzi w skladnik do porownania"
+	print "compa.py -g <filename> , --genere <filename>\t==> Program tworzy poprawiony plik wzgledem oryginalu"
+	print "compa.py -o <filename> , --output-log <filename>\t==> Utworzenie pliku z logami zmian"
+	print "compa.py -d , --debug\t==> Wlacza dodatkowe informacje (zwiazane z praca programu, nie wykonywanymi zadaniami)"
 	print 
-	print u"Przyk³ady: "
+	print "Przyklady: "
 	print 
-	print u"Bez liczenia lini, generuj plik z logami:"
-	print u"compa.py -n -o log.txt org.txt tocompare.txt"
+	print "Bez liczenia lini, generuj plik z logami:"
+	print "compa.py -n -o log.txt org.txt tocompare.txt"
 	print
-	print u"Z liczeniem lini, z generacj¹ poprawionego pliku, z generowaniem pliku z logami"
-	print u"compa.py -l -g improve.txt -o log.txt org.txt tocompare.txt"
-	print u"compa.py -g improve.txt -o log.txt org.txt tocompare.txt"
+	print "Z liczeniem lini, z generacja poprawionego pliku, z generowaniem pliku z logami"
+	print "compa.py -l -g improve.txt -o log.txt org.txt tocompare.txt"
+	print "compa.py -g improve.txt -o log.txt org.txt tocompare.txt"
 	print 
-	print u"Zwyk³e wyœwietlenie ró¿nic (ró¿nica po ró¿nicy)"
-	print u"compa.py org.txt tocompare.txt"
+	print "Zwykle wyswietlenie roznic (roznica po roznicy)"
+	print "compa.py org.txt tocompare.txt"
 	print
 	print "..........:::::::::: COMPA - RER ::::::::::.........."
 
@@ -56,7 +56,7 @@ def Compare( org_line, comp_line ):
 	len_comp_line = len(comp_line)
 	
 	if len_org_line != len_comp_line:
-		print u"[ !!! ] Ró¿nica w d³ugoœci lini - coœ siê nie zgadza\t\t...WARNING"
+		print "[ !!! ] Roznica w dlugosci linii - cos sie nie zgadza\t\t...WARNING"
 		flag_equals_len_line = False
 	
 	len_greater_line = len_org_line;
@@ -74,7 +74,7 @@ def Compare( org_line, comp_line ):
 	# TUTAJ JEST COŒ ZJEBANE
 	for i in range(len_greater_line):
 		if org_line[i] != comp_line[i]:
-			print u"Znalaz³em %i ró¿nicê! Wyraz oryginalny: %s != %s :Wyraz porównawczy. Literka %s != %s" % ( i, "<bd>", "<bd>", org_line[i], comp_line[i])
+			print "Znalazlem %i roznice! Wyraz oryginalny: %s != %s :Wyraz porownawczy. Literka %s != %s" % ( i, "<bd>", "<bd>", org_line[i], comp_line[i])
 			counter_difrence += 1
 			Difrence.append(comp_line)
 			
@@ -85,61 +85,80 @@ def CompareTwo( org_line, comp_line ):
 	global Difrence
 	global counter_difrence
 	
-	len_org_line = len(org_line)
-	len_comp_line = len(comp_line)
+	#len_org_line = len(org_line)
+	#len_comp_line = len(comp_line)
 	
-	if len_org_line != len_comp_line:
-		print u"[ !!! ] Ró¿nica w d³ugoœci lini - coœ siê nie zgadza\t\t...WARNING"
-		flag_equals_len_line = False
+	#if len_org_line != len_comp_line:
+	#	print "[ !!! ] Roznica w dlugosci lini - cos sie nie zgadza\t\t...WARNING"
+	#	flag_equals_len_line = False
 	
-	len_greater_line = len_org_line;
-	if not flag_equals_len_line:
-		if len_org_line > len_comp_line:
-			len_greater_line = len_org_line
-		elif len_comp_line > len_org_line:
-			len_greater_line = len_org_line
-	if DEBUG:
-		print "len_greater_line = %i" % len_greater_line
-		print "len_org_line = %i" % len_org_line
-		print "len_comp_line = %i" % len_comp_line
-		print "CHUJ!\n"
+	#len_greater_line = len_org_line;
+	#if not flag_equals_len_line:
+	#	if len_org_line > len_comp_line:
+	#		len_greater_line = len_org_line
+	#	elif len_comp_line > len_org_line:
+	#		len_greater_line = len_org_line
+
+	#if DEBUG:
+	#	print "len_greater_line = %i" % len_greater_line
+	#	print "len_org_line = %i" % len_org_line
+	#	print "len_comp_line = %i" % len_comp_line
+	#	print "CHUJ!\n"
 	
 	words_org = org_line.split()
 	words_compa = comp_line.split()
-	len_wrods_org = len(words_org)
+	len_words_org = len(words_org)
 	len_words_compa = len(words_compa)
 
-	greater_len_words = GetGreater(len_wrods_org, len_words_compa)
-	if len_wrods_org != greater_len_words:
-		print "[ WARNING ] Ró¿cnia w iloœci s³ów!"
+	greater_len_words = GetGreater(len_words_org, len_words_compa)
+	if len_words_org != len_words_compa:
+		print "[ WARNING ] Rozcnia w ilosci slow!"
+		if len_words_org < len_words_compa:
+			for i in range( (len_words_compa - len_words_org) ):
+				words_org.append(" <org>")
+				if DEBUG:
+					print "Dodalem slowko w tablicy org"
+		else:
+			for i in range( (len_words_org - len_words_compa) ):
+				words_compa.append(" <compa>")
+				if DEBUG:
+					print "Dodalem slowko w tablicy comp"
 			
-	# TUTAJ!
+
+	for i in range(greater_len_words):
+		if words_compa[i] != words_org[i]:
+			counter_difrence += 1
+			print "[ ROZNICA ] %i ==> oryginal: %s vs. %s :porownawczy\t\t...WARNING" % (counter_difrence, words_org[i], words_compa[i])
+			Difrence.append( ("Roznica %i: %s vs. %s" % (counter_difrence, words_org[i], words_compa[i]) ) )
+		
+	if DEBUG:
+		print "Sprawdzono wszystkie dane w wierszu - czekam na nastepny\t\t...OK"
 	return
 
 def Summary():
-	print u"Znalezionych ró¿nic: %i\n" % counter_difrence
-	print u"Lista ró¿nic (wyœwietlane s¹ linie z pliku do porównania, nie z orygina³u!):\n"
+	print "Znalezionych roznic: %i\n" % counter_difrence
+	print "Lista roznic (wyswietlane sa linie z pliku do porownania, nie z oryginalu!):\n"
 	for i in range(len(Difrence)):
-		print u"%i: %s" % (i, Difrence[i])
-	print u"\nKoniec."
+		print "%i: %s" % (i, Difrence[i])
+	print "\nKoniec."
 	
 	
 def OpenFile(fname):
 	try:
 		tempFile = open(fname, 'r' )
-		print u"[   LOG ] OpenFile succesfuly opened!\t\t...OK\n"
+		print "[   LOG ] OpenFile succesfuly opened!\t\t...OK\n"
 		return tempFile
 	except IOError as e:
-		print u"OpenFile Function failed: %s\n" % e
+		print "OpenFile Function failed: %s\n" % e
 		sys.exit()
 		
 def SaveFile(fname):
 	try:
 		tempFile = open(fname, 'w')
-		print u"[   LOG ] SaveFile succesfuly opened!\t\t...OK\n"
+		print "[   LOG ] SaveFile succesfuly opened!\t\t...OK\n"
 		return tempFile
 	except IOError as e:
-		print u"SaveFile function failed: %s\n" % e
+		print "SaveFile function failed: %s\n" % e
 		sys.exit()
 		
 def CounterLines(file):
@@ -161,7 +180,7 @@ def GetGreater( a, b ):
 		return b
 	elif a == b:
 		if DEBUG:
-			print "Parametry s¹ równe: %i == %i" % (a, b)
+			print "Parametry sa rowne: %i == %i" % (a, b)
 		return a
 
 def GetSmaller( a, b ):
@@ -175,7 +194,7 @@ def GetSmaller( a, b ):
 		return a
 	elif a == b:
 		if DEBUG:
-			print "Parametry s¹ równe: %i == %i" % (a, b)
+			print "Parametry sa rowne: %i == %i" % (a, b)
 		return a
 	
 def main(argv):
@@ -189,7 +208,7 @@ def main(argv):
 	try:
 		opts, args = getopt.getopt(argv, "hnlg:o:d", ["help","no-line","line-on","genere=", "output-log=", "debug"] )
 	except  getopt.GetoptError:
-		print u"Fejjjlaaaa something was wrong in getopt function"
+		print "Fejjjlaaaa something was wrong in getopt function"
 		sys.exit(2)
 		
 	for opt, arg in opts:
@@ -199,18 +218,18 @@ def main(argv):
 			sys.exit()
 		elif opt in ("-n", "--no-line"):
 			# Flaga: nie bierz pod uwagê linii
-			print u"Nie biorê pod uwagê numeracji linii\n"
+			print "Nie biore pod uwage numeracji linii\n"
 			#sys.exit()
 		elif opt in ("-l", "--line-on"):
 			# Flaga bierz pod uwage liniê
-			print u"Biorê pod uwagê numeracjê linii\n"
+			print "Biore pod uwage numeracje linii\n"
 			#sys.exit()
 		elif opt in ("-g", "--genere"):
 			# Generuj plik wyjœciowy
 			fn_g = arg
-			print u"Nazwa pliku do wygenerowania %s\n" % fn_g
+			print "Nazwa pliku do wygenerowania %s\n" % fn_g
 			if fn_g == "":
-				print u"Nie podano nazwy pliku dla wygenerowania poprawionego pliku\n"
+				print "Nie podano nazwy pliku dla wygenerowania poprawionego pliku\n"
 				sys.exit()
 		elif opt in ("-o", "--output-log"):
 			# Generuj plik wyjœciowy
@@ -218,47 +237,49 @@ def main(argv):
 		elif opt in ("-d", "--debug"):
 			# Generuj plik wyjœciowy
 			DEBUG = True
-			print u"Tryb informacji debuguj¹cych w³¹czony!\n"
+			print "Tryb informacji debugujacych wlaczony!\n"
 	
 	argv_len = len(argv) - 1
 	if len(argv) > 1:
 		if DEBUG:
-			print u"Wartoœæ argv od 1: %s\n" % argv
+			print "Wartosc argv od 1: %s\n" % argv
 		tempArg = argv[argv_len-1]
 		if tempArg[0] != '-':
 			fn_f = argv[argv_len-1]
 			fn_s = argv[argv_len]
 	else:
-		print u"Nie podano argumentów!\n"
+		print "Nie podano argumentow!\n"
 		print_help()
 		sys.exit()
 	
 	if fn_f == "" or fn_s == "":
-		print u"Nie podano nazw plików\n"
+		print "Nie podano nazw plikow\n"
 		sys.exit(2)
 	
 	if DEBUG:
-		print u"[   LOG ] fn_f = %s : fn_s = %s : fn_g = %s : fn_l = %s\n" % (fn_f, fn_s, fn_g, fn_l)
+		print "[   LOG ] fn_f = %s : fn_s = %s : fn_g = %s : fn_l = %s\n" % (fn_f, fn_s, fn_g, fn_l)
 	
 	# Otwarcie plików "g³ównych" - wzorca i do porównania
 	File_ORG = OpenFile(fn_f)
 	File_COMP = OpenFile(fn_s)
 	if DEBUG:
-		print u"Otwarcie pliku sie powiod³o\n"
+		print "Otwarcie pliku sie powiodlo\n"
 		
 	try:
 		num_line_org = CounterLines(File_ORG)
 		num_line_comp = CounterLines(File_COMP)
 		greater_line = 0
+		flag_not_equals_line = False
 		
 		if num_line_org != num_line_comp:
-			print u"[ WARNING ] Liczba linii w plikach jest ró¿na!"
+			print "[ WARNING ] Liczba linii w plikach jest rozna!"
+			flag_not_equals_line = True
 		
 		if DEBUG:
-			print u"NUM LINE ORG = %i" % num_line_org
-			print u"NUM LINE COMP = %i" % num_line_comp
+			print "NUM LINE ORG = %i" % num_line_org
+			print "NUM LINE COMP = %i" % num_line_comp
 			
-		greater_line = GetSmaller(num_line_org, num_line_comp)
+		greater_line = GetGreater(num_line_org, num_line_comp)
 		if DEBUG:
 			print "Greater Line = %i" % greater_line
 		
@@ -272,6 +293,12 @@ def main(argv):
 		
 		for linecomp in File_COMP:
 			lines_compare_file.append(linecomp)
+			
+		if flag_not_equals_line:
+			if num_line_org < num_line_comp:
+				lines_original_file.append("<levelingline>")
+			if num_line_comp < num_line_org:
+				lines_compare_file.append("<levelingline>")
 		
 		if DEBUG:
 			print "______________________ ORG LINE: "
@@ -286,7 +313,7 @@ def main(argv):
 		
 	except ValueError as e:
 		if DEBUG:
-			print u"[   ERROR LOG ] Nie uda³o sie odczyatæ z pliku: %s" % e
+			print "[   ERROR LOG ] Nie udalo sie odczyatc z pliku: %s" % e
 		
 	Summary()
 	File_ORG.close()
